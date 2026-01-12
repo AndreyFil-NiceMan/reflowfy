3251f4ee53ca928e54870a00a0d4c8e3ae16fe93
kofli koko <koflinew@gmail.com>
2026-01-13 00:04:36 +0200

    feat: add source and destination factories
    - Add SourceFactory for creating sources by type name
    - Add DestinationFactory for creating destinations by type name
    - Auto-register built-in sources (elastic, sql, mock)
    - Auto-register built-in destinations (kafka, http, console)

---
+++ reflowfy/factories/__init__.py
@@ -0,0 +1,9 @@
+"""Source and destination factory classes for dynamic instantiation."""
+
+from reflowfy.factories.source_factory import SourceFactory
+from reflowfy.factories.destination_factory import DestinationFactory
+
+__all__ = [
+    "SourceFactory",
+    "DestinationFactory",
+]
