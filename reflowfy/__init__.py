141bc6b8dbd8cf9e80f8b07aa9352e4f8df23219
kofli <andreyfilwork@gmail.com>
2026-06-22 15:26:08 +0300

    release: 1.0.13

--- reflowfy/__init__.py
+++ reflowfy/__init__.py
@@ -24,7 +24,7 @@ from reflowfy.sources.decorators import source, source_registry
 from reflowfy.destinations.decorators import destination, destination_registry
 from reflowfy.transformations.decorators import transformation
 
-__version__ = "1.0.12"
+__version__ = "1.0.13"
 
 __all__ = [
     "AbstractPipeline",
